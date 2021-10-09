615c44e51f6b9ba9d1ac74b539fc96af24b2eeb4
OlegAndrus12 <andrusoleg123@gmail.com>
2021-10-09 13:45:16 +0300

    login page has been added

--- social_network/main/urls.py
+++ social_network/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="welcome"),
+    path("feeds/", views.feeds, name="feeds")
 ]
--- social_network/main/views.py
+++ social_network/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "main/welcome.html")
+
+def feeds(request):
+    return render(request, "main/feeds.html")
