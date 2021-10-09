c24dda7c37e6d377e0bef85ea4dc43b10abd369f
OlegAndrus12 <andrusoleg123@gmail.com>
2021-10-09 11:54:55 +0300

    models are created

--- social_network/main/models.py
+++ social_network/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    thumbnails = models.IntegerField()
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.title
